1ab39caaa071451b0e55d9665dd1b579903e5804
bearcat-panda <1411131632@qq.com>
2021-02-02 16:54:45 +0800

    bearcat-panda  2021/2/2  连个正序数组的中位数

--- 2.两数相加/addTwoNumbers_test.go
+++ 2.两数相加/addTwoNumbers_test.go
@@ -53,6 +53,16 @@ func TestAddTwoNumbers(t *testing.T) {
 		}
 	}
 
+	result = addTwoNumberOne(l1, l2)
+
+	for {
+		fmt.Println(result.Val)
+		result = result.Next
+		if result == nil {
+			return
+		}
+	}
+
 }
 
 type ListNode struct {
@@ -180,3 +190,53 @@ func addTwoNumbersBest(l1 *ListNode, l2 *ListNode) *ListNode {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+
+//方法一:模拟
+/**
+思路和算法
+	由于输入的两个链表都是逆序存储数字的位数的,因此两个链表中同一
+位置的数字可以直接相加. 我们同时遍历两个链表,逐位计算他们的和,
+并与当前位置的进位值相加.具体而言,如果当前两个链表处相应位置
+的数字n1,n2,进位值为carry,而新的进位值为(n1+n2+carry)/10
+	如果两个链表的长度不同,则可以认为长度短的链表后面有若干个0.
+此外,如果链表遍历结束后,有carry>0,还需要在答案链表后面附加一个节点,节点的值为carry
+*/
+
+func addTwoNumberOne(l1, l2 *ListNode) (head *ListNode) {
+	var tail *ListNode
+	var carry int
+
+	for l1 != nil || l2 != nil || carry != 0 {
+		var v1, v2 int
+		if l1 != nil {
+			v1 = l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			v2 = l2.Val
+			l2 = l2.Next
+		}
+
+		sum := v1 + v2 + carry
+		sum, carry = sum%10, sum/10
+		if head == nil {
+			head = &ListNode{Val: sum}
+			tail = head
+
+		} else {
+			tail = &ListNode{Val: sum}
+			tail = tail.Next
+		}
+
+	}
+
+	return head
+}
+
+//复杂度分析
+/**
+.时间复杂度:O(max(m,n)), 其中m,n为两个链表的长度.我们遍历两个链表的全部位置,
+而处理每个位置只需要O(1)的时间
+.空间复杂度:O(max(m,n)).答案链表的长度最多为较长链表的长度+1
+*/
--- 3.无重复字符串的最长子串/lengthOfLongestSubstring_test.go
+++ 3.无重复字符串的最长子串/lengthOfLongestSubstring_test.go
@@ -58,6 +58,7 @@ func TestTengthOfLongestSubstring(t *testing.T) {
 	var s = "pwwkew"
 	length := lengthOfLongestSubstring(s)
 	length = lengthOfLongestSubstringBit(s)
+	length = lengthOfLongesSubstring(s)
 	fmt.Println(length)
 
 	s = "abcabcbb"
@@ -174,15 +175,73 @@ func max(a int, b int) int {
 
 //leetcode submit region end(Prohibit modification and deletion)
 
-/*apiv1 := router.Group("/api/version1")
-{
-	//测试项目状态
-	apiv1.GET("ping", Ping)
+//方法一:双指针
+/**
+思路和算法
+	我们先用一个例子考虑如何在较优的时间复杂度内通过本题.
+	我们不妨以示例一中的字符串abcabcbb为例,找出每一个字符开始的,不包含
+重复字符串的最长子串,那么其中最长的那个字符串即为答案.对于示例一中的字符串,
+我们列举出这些结果,其中括号中表示选中的字符以及最长的字符串:
+	以 (a)bcabcbb}(a)bcabcbb 开始的最长字符串为 (abc)abcbb}(abc)abcbb；
+	以 a(b)cabcbb}a(b)cabcbb 开始的最长字符串为 a(bca)bcbb}a(bca)bcbb；
+	以 ab(c)abcbb}ab(c)abcbb 开始的最长字符串为 ab(cab)cbb}ab(cab)cbb；
+	以 abc(a)bcbb}abc(a)bcbb 开始的最长字符串为 abc(abc)bb}abc(abc)bb；
+	以 abca(b)cbb}abca(b)cbb 开始的最长字符串为 abca(bc)bb}abca(bc)bb；
+	以 abcab(c)bb}abcab(c)bb 开始的最长字符串为 abcab(cb)b}abcab(cb)b；
+	以 abcabc(b)b}abcabc(b)b 开始的最长字符串为 abcabc(b)b}abcabc(b)b；
+	以 abcabcb(b)}abcabcb(b) 开始的最长字符串为 abcabcb(b)}abcabcb(b)。
 
-	tagRouter := apiv1.Group("/tag")
-	tag := v1.NewTag()
-	{
-		tagRouter.Get("/list", tag.Get)
+	发现了什么?如果我们依次递增的枚举子串的起始位置,那么子串的结束位置也是递增的!
+这里的原因在于,假设我们选择字符串中的第K个字符作为起始位置,并且得到了不包含重复
+字符的最长子串的结束位置为rk.那么当我们选择第k+1个字符作为起始位置时,首先从k+1到
+rk的字符显示是不重复的,并且由于少了原本的第k个字符,我们可以尝试继续增大rk,直到右侧
+出现了重复字符为止.
+	这样一来,我们就可以使用[双指针]来解决这个问题了:
+	.我们使用两个指针表示字符串的某个子串(的左右边界).其中左指针代表着上文中[枚举子串的起始位置],
+而右指针即为上文中的rk:
+	.在每一步的操作中,我们会将左指针向右移动一格,表示我们开始枚举下一个字符作为起始位置,然后我们
+可以不断地向右移动右指针,但需要保证这两个指针对应的子串中没有重复的字符.在移动结束后,这个子串就对应
+着以左指针开始的,不包含重复字符的最长子串.我们记录下这个子串的长度;
+	.在枚举结束后,我们找到的最长的子串的长度即为答案
+在上面的流程中，我们还需要使用一种数据结构来判断 是否有重复的字符，常用的数据结构为哈希集合（即 C++ 中的 std::unordered_set，
+Java 中的 HashSet，Python 中的 set, JavaScript 中的 Set）。在左指针向右移动的时候，我们从哈希集合中移除一个字符，在右指针向右
+移动的时候，我们往哈希集合中添加一个字符。
+
+至此，我们就完美解决了本题。
+
+
+*/
+func lengthOfLongesSubstring(s string) int {
+	//哈希集合,记录每个字符是否出现过
+	m := map[byte]int{}
+	n := len(s)
+
+	//rk右指针,初始值为-1,相当于我们在字符串的左侧,还没有开始移动
+	//ans最长字符串的长度
+	rk, ans := -1, 0
+
+	for i := 0; i < n; i++ {
+		if i != 0 {
+			//左指针向右移动一格,移除一格字符
+			delete(m, s[i-1])
+		}
+		//右指针向右移动.并且当前字符不存在
+		for rk+1 < n && m[s[rk+1]] == 0 {
+			//不断的向右移动指针
+			m[s[rk+1]]++ //当前字符的值不为0
+			rk++
+		}
+		//第i到rk个字符是一个极长的无重复字符子串
+		ans = max(ans, rk-i+1)
+
 	}
+	return ans
+}
 
-}*/
+//复杂度分析
+/**
+.时间复杂度:O(N),其中N是字符串的长度.左指针和右指针分别会遍历整个字符串一次.
+.空间复杂度:O(|Σ|),其中Σ表示字符集(及字符串中可以出现的字符),|Σ|表示字符集的大小.
+在本题中没有明确说明字符集,因此可以默认为所有ASCll码在[0,128)内的字符,即|Σ|=128.
+我们需要用到哈希集合来存储出现过的字符,而字符最多有|Σ|个,因此空间复杂度为O(|Σ|)
+*/
